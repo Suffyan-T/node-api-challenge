9536b388554acb4b874ac27d21f3425b1416bdea
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-03-13 11:21:45 -0700

    added content

--- api/server.js
+++ api/server.js
@@ -7,8 +7,9 @@ const actionsRouter = require('../routers/actionsRouter');
 
 const server = express();
 server.use(express.json());
-server.use('/projects', [helmet(), logger, projectsRouter]);
-server.use('/actions', [helmet(), logger, actionsRouter]);
+server.use(logger);
+server.use('/projects', projectsRouter);
+server.use('/actions', actionsRouter);
 
 
 server.get('/', (req, res) => {
